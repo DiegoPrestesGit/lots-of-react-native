392013305a759aa05daa2e609fac80739627fa49
Diego Prestes <diegoprestes.git@hotmail.com>
2021-09-07 13:16:30 -0300

    feat: add more linting stuff

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -3,18 +3,26 @@ import { Text, StyleSheet, View, FlatList } from 'react-native'
 
 const ListScreen = () => {
   const nameArr = [
-    { name: 'John', key2: '1' },
-    { name: 'Wick', key2: '2' },
-    { name: 'Baba', key2: '3' },
-    { name: 'Yaga', key2: '4' },
+    { name: 'John', age:20, key2: '1' },
+    { name: 'Wick', age:30, key2: '2' },
+    { name: 'Baba', age:25, key2: '3' },
+    { name: 'Yaga', age:26, key2: '4' },
+    { name: 'Yaga', age:23, key2: '5' },
+    { name: 'Yaga', age:14, key2: '64' },
+    { name: 'Yaga', age:63, key2: '74' },
+    { name: 'Yaga', age:10, key2: '84' },
+    { name: 'Yaga', age:3, key2: '94' },
+    { name: 'Yaga', age:54, key2: '34' },
+
   ]
 
   const renderNames = ({ item }) => {
-    return <Text>{item.name}</Text>
+    return <Text style={styles.textStyle}>{item.name} has {item.age} years old</Text>
   }
 
   return (
     <FlatList
+      showsVerticalScrollIndicator={false}
       keyExtractor={ item => item.key2 }
       data={nameArr}
       renderItem={renderNames}
@@ -23,7 +31,10 @@ const ListScreen = () => {
 }
 
 const styles = StyleSheet.create({
-
+  textStyle: {
+    marginVertical: 50,
+    marginHorizontal: 5
+  }
 })
 
 export default ListScreen
