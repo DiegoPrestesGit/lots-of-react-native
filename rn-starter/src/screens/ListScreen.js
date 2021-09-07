d360e6f54055e1a22a423671b16ef954c4b4c838
Diego Prestes <diegoprestes.git@hotmail.com>
2021-09-07 12:35:58 -0300

    feat: add more list stuff

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -3,14 +3,22 @@ import { Text, StyleSheet, View, FlatList } from 'react-native'
 
 const ListScreen = () => {
   const nameArr = [
-    { name: 'John' },
-    { name: 'Wick' },
-    { name: 'Baba' },
-    { name: 'Yaga' },
+    { name: 'John', key2: '1' },
+    { name: 'Wick', key2: '2' },
+    { name: 'Baba', key2: '3' },
+    { name: 'Yaga', key2: '4' },
   ]
 
+  const renderNames = ({ item }) => {
+    return <Text>{item.name}</Text>
+  }
+
   return (
-    <Text>List Screen</Text>
+    <FlatList
+      keyExtractor={ item => item.key2 }
+      data={nameArr}
+      renderItem={renderNames}
+    />
   )
 }
 
